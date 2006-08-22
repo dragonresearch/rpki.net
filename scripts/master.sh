16cae687d82b8f0d6c57df398dc0d31463ac099b
Rob Austein <sra@hactrn.net>
2006-08-22 22:28:28 +0000

    # on 2006/08/10 00:18:44, sra did: apnic trust anchor
    svn path=/scripts/master.sh; revision=195

--- scripts/master.sh
+++ scripts/master.sh
@@ -7,7 +7,7 @@ cd `dirname $0`
 perl gen-verify-test.pl		\
     ca-trial.ripe.net/RIPE	\
     ca-trial.ripe.net/ARIN	\
-    repository.apnic.net/APNIC	|
+    repository.apnic.net	|
 tee test.sh |
 sh 2>&1 |
 tee test.log

4a6a95015a09560b8928cea70abe852135125a39
Rob Austein <sra@hactrn.net>
2006-08-22 22:31:01 +0000

    # on 2006/08/18 00:25:02, sra did: save awk
    svn path=/scripts/check-hashes.sh; revision=217

--- scripts/check-hashes.sh
+++ scripts/check-hashes.sh
@@ -7,5 +7,9 @@ cd `dirname $0`
 : ${switches='-verbose -crl_check_all -policy_check -explicit_policy -policy 1.3.6.1.5.5.7.14.2 -x509_strict'}
 : ${hashtree=hashed}
 
-find $hashtree -type f -name '*.[0-9]*' 2>&1 -exec \
+find $hashtree -type f -name '*.[0-9]*' 2>&1 -print -exec \
     $openssl verify $switches -CApath $(find $hashtree -type d | tr \\012 : | sed 's=:$==') {} \;
+
+# Hack for analyzing results of running this script:
+#
+# awk -F: '/^hashed/ && NF == 1 {f = $0; p = 1; next} /^hashed/ && NF == 2 && $1 == f && $2 == " OK" {next} p {print "\n" f; p = 0} {print}' check-hashes.log

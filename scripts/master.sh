a187136528d6860e71585c8c0727b9442dee4f78
Rob Austein <sra@hactrn.net>
2006-08-22 22:28:07 +0000

    # on 2006/08/09 23:32:57, sra did: Initial revision
    svn path=/scripts/master.sh; revision=192

---
+++ scripts/master.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -
+# $Id$
+
+cd `dirname $0`
+. mirror-apnic.sh
+. mirror-ripe.sh
+perl gen-verify-test.pl ca-trial.ripe.net/{RIPE,ARIN} repository.apnic.net/APNIC |
+tee test.sh |
+sh 2>&1 |
+tee test.log

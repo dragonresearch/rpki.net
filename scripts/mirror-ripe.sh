e2d8549b793fbab343efdfc6ade1845973cb8d62
Rob Austein <sra@hactrn.net>
2006-08-22 22:27:49 +0000

    # on 2006/08/09 20:24:47, sra did: Initial revision
    svn path=/scripts/mirror-ripe.sh; revision=189

---
+++ scripts/mirror-ripe.sh
@@ -0,0 +1,48 @@
+#!/bin/sh -
+# $Id$
+
+cd `dirname $0`
+
+# The following are freshly produced in conformance with Geoff &
+# George's latest developments - I hope.
+
+# Trust Anchors (consider this an out-of-band delivery method):
+
+# RIPE TA:
+fetch -m -o riperoot/repos/root.cer \
+    http://ca-trial.ripe.net/~riperoot/repos/root.cer
+
+# ARIN TA: 
+fetch -m -o arinroot/repos/root.cer \
+    http://ca-trial.ripe.net/~arinroot/repos/root.cer
+
+# The repositories are here:
+rsync -aiz --delete rsync://ca-trial.ripe.net/RIPE/ RIPE/
+rsync -aiz --delete rsync://ca-trial.ripe.net/ARIN/ ARIN/
+
+# Some test certificates:
+
+# RIPE->RIPE->ISP: 
+fetch -m -o ripeprod/repos/ripe-08.cer \
+    http://ca-trial.ripe.net/~ripeprod/repos/ripe-08.cer
+
+# ARIN->ARIN->ISP: 
+fetch -m -o arinprod/repos/arin-01.cer \
+    http://ca-trial.ripe.net/~arinprod/repos/arin-01.cer
+
+# RIPE->ARIN->ISP: 
+fetch -m -o arinprod/repos/ripe-01.cer \
+    http://ca-trial.ripe.net/~arinprod/repos/ripe-01.cer
+
+# ARIN->RIPE->ISP: 
+fetch -m -o ripeprod/repos/arin-01.cer \
+    http://ca-trial.ripe.net/~ripeprod/repos/arin-01.cer
+
+# I think they work with full up-down chaining, provided that I copied 
+# everything in place.
+#
+# George, please look at these, I believe I only need your SIA for these to be 
+# ready:
+#
+# RIPE->APNIC cert currently: http://ca-trial.ripe.net/~riperoot/repos/root-0E.cer
+# ARIN->APNIC cert currently: http://ca-trial.ripe.net/~arinroot/repos/root-09.cer

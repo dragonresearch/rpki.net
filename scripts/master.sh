5aa91a6daa34b3542248f5713389d97729c547e8
Rob Austein <sra@hactrn.net>
2006-08-22 22:28:14 +0000

    # on 2006/08/09 23:35:04, sra did: forgot which shell i was running here
    svn path=/scripts/master.sh; revision=193

--- scripts/master.sh
+++ scripts/master.sh
@@ -4,7 +4,10 @@
 cd `dirname $0`
 . mirror-apnic.sh
 . mirror-ripe.sh
-perl gen-verify-test.pl ca-trial.ripe.net/{RIPE,ARIN} repository.apnic.net/APNIC |
+perl gen-verify-test.pl		\
+    ca-trial.ripe.net/RIPE	\
+    ca-trial.ripe.net/ARIN	\
+    repository.apnic.net/APNIC	|
 tee test.sh |
 sh 2>&1 |
 tee test.log

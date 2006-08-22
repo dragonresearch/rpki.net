d9f750ac29af074e92ebd24ac0d56680381d30b2
Rob Austein <sra@hactrn.net>
2006-08-22 22:28:52 +0000

    # on 2006/08/10 06:54:08, sra did: *** empty log message ***
    svn path=/scripts/uri.pl; revision=198

--- scripts/uri.pl
+++ scripts/uri.pl
@@ -6,22 +6,16 @@ eval 'exec perl -w -S $0 ${1+"$@"}'
 while (@ARGV) {
     my $file = shift(@ARGV);
     my ($aia, $sia, $crl, $a, $s, $c) = qw(- - -);
-    if ($file =~ /\.cer$/) {
-	open(F, "-|", qw(openssl x509 -noout -inform DER -text -in), $file)
-	    or die("Couldn't run openssl x509 on $file: $!\n");
-    } elsif  ($file =~ /\.crl$/) {
-	open(F, "-|", qw(openssl crl  -noout -inform DER -text -in), $file)
-	    or die("Couldn't run openssl x509 on $file: $!\n");
-    } else {
-	next;
-    }
+    next unless ($file =~ /\.cer$/);
+    open(F, "-|", qw(openssl x509 -noout -inform DER -text -in), $file)
+	or die("Couldn't run openssl x509 on $file: $!\n");
     while (<F>) {
 	chomp;
 	s{^.+URI:}{};
 	$a = $. + 1
-	    if (/X509v3 Authority Information Access:/);
+	    if (/Authority Information Access:/);
 	$s = $. + 1
-	    if (/X509v3 Subject Information Access:/);
+	    if (/Subject Information Access:/);
 	$c = $. + 1
 	    if (/X509v3 CRL Distribution Points:/);
 	$aia = $_

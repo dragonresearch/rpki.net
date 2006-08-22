0e6de91b9d13c7519d61cfa1a930b6e9c0431797
Rob Austein <sra@hactrn.net>
2006-08-22 22:44:27 +0000

    Reorganize
    svn path=/scripts/master.sh; revision=222

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,13 +1,24 @@
 #!/bin/sh -
 # $Id$
 
+scripts="subvert-rpki.hactrn.net/scripts"
+
+repositories="ca-trial.ripe.net/RIPE ca-trial.ripe.net/ARIN repository.apnic.net"
+
 cd `dirname $0`
-. mirror-apnic.sh
-. mirror-ripe.sh
-perl gen-verify-test.pl		\
-    ca-trial.ripe.net/RIPE	\
-    ca-trial.ripe.net/ARIN	\
-    repository.apnic.net	|
-tee test.sh |
+
+. $scripts/mirror-apnic.sh
+. $scripts/mirror-ripe.sh
+
+perl $scripts/gen-verify-test.pl $repositories |
+tee verify.sh |
 sh 2>&1 |
-tee test.log
+tee verify.log
+
+perl $scripts/make-hashes.pl $repositories |
+tee make-hashes.sh |
+sh 2>&1 |
+tee make-hashes.log
+
+sh $scripts/check-hashes.sh 2>&1 |
+tee check-hashes.log

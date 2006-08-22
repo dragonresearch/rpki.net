bf44331cca85a3e6acd001f1c114e4da146e4885
Rob Austein <sra@hactrn.net>
2006-08-22 22:30:39 +0000

    # on 2006/08/15 18:43:32, sra did: allow veriable overrides
    svn path=/scripts/check-hashes.sh; revision=214

--- scripts/check-hashes.sh
+++ scripts/check-hashes.sh
@@ -3,9 +3,9 @@
 
 cd `dirname $0`
 
-openssl=/u/sra/isc/route-pki/subvert-rpki.hactrn.net/openssl/trunk/apps/openssl
+: ${openssl=/u/sra/isc/route-pki/subvert-rpki.hactrn.net/openssl/trunk/apps/openssl}
+: ${switches='-verbose -crl_check_all -policy_check -explicit_policy -policy 1.3.6.1.5.5.7.14.2 -x509_strict'}
+: ${hashtree=hashed}
 
-capth=$(echo $(find hashed -type d) | tr \\040 :)
-
-find hashed -type f -name '*.?' |
-xargs $openssl verify -verbose -crl_check_all -policy_check -explicit_policy -policy 1.3.6.1.5.5.7.14.2 -x509_strict -CApath $capth 
+find $hashtree -type f -name '*.[0-9]*' 2>&1 -exec \
+    $openssl verify $switches -CApath $(find $hashtree -type d | tr \\012 : | sed 's=:$==') {} \;

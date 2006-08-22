d714d7d7ad6dea084b66d50cc3c81328ba4e209c
Rob Austein <sra@hactrn.net>
2006-08-22 22:26:37 +0000

    # on 2006/08/09 06:06:15, sra did: *** empty log message ***
    svn path=/scripts/mirror-apnic.sh; revision=179

--- scripts/mirror-apnic.sh
+++ scripts/mirror-apnic.sh
@@ -3,8 +3,4 @@
 
 cd `dirname $0`
 
-# old repository, no longer maintained
-# wget --mirror --relative --no-parent ftp://ftp.apnic.net/pub/test-certs/
-
-# new repository, theoretically maintained
-rsync -avz rsync://repository.apnic.net/APNIC/ repository.apnic.net/APNIC/
+rsync --archive --itemize-changes --compress --delete rsync://repository.apnic.net/APNIC/ repository.apnic.net/APNIC/

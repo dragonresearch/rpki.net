2c44014b3664de3ea9445fede68afbde535cf503
Rob Austein <sra@hactrn.net>
2007-01-30 20:29:39 +0000

    Initial.
    svn path=/scripts/regeng-api.lisp; revision=473

---
+++ scripts/regeng-api.lisp
@@ -0,0 +1,6 @@
+;;; $Id$
+;;;
+;;; Scratch pad for working out API design for registration engine.
+;;;
+;;; This file is psuedocode, I just wanted to take advantage of
+;;; emacs's built-in support for languages with reasonable syntax.

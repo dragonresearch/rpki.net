fa1bf88f7f2d026784b8430de711885cfde0fd8e
Rob Austein <sra@hactrn.net>
2007-08-08 16:26:38 +0000

    Checkpoint
    svn path=/scripts/http-server.py; revision=844

--- scripts/http-server.py
+++ scripts/http-server.py
@@ -2,11 +2,16 @@
 
 import rpki.https, tlslite.api
 
-if False:
-  certInfo = rpki.https.CertInfo("Carol")
-else:
-  certInfo = rpki.https.CertInfo()
+certInfo = rpki.https.CertInfo("Carol")
 
+# Ok, here's the problem: the certChain is order-sensitive.
+# We have to put the EE cert before the CA cert or things break.
+#
+# For the moment we can kludge this but in the general case we're
+# going to have to sort certs somehow.  This is the second time this
+# problem has come up (CMS had the same issue).
+
+if True:
   certChain = []
   for file in ("biz-certs/Carol-EE.cer", "biz-certs/Carol-CA.cer"):
     f = open(file, "r")
@@ -15,10 +20,6 @@ else:
     f.close()
     certChain.append(x509)
   certInfo.certChain = tlslite.api.X509CertChain(certChain)
-
-  f = open("biz-certs/Carol-EE.key", "r")
-  certInfo.privateKey = tlslite.api.parsePEMKey(f.read(), private=True)
-  f.close()
 
 def handler(self, query):
   return 200, "I got:\n" + query

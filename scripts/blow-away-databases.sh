4835dba39e444b0bf9b723d7214a0e13963eac81
Rob Austein <sra@hactrn.net>
2007-09-14 21:51:29 +0000

    New tool
    svn path=/scripts/blow-away-databases.sh; revision=960

---
+++ scripts/blow-away-databases.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -
+# $Id$
+
+echo "This script destroys and rebuilds our databases."
+echo "Don't type the password unless you're sure you want to do this."
+
+(echo 'DROP DATABASE rpki; CREATE DATABASE rpki; USE rpki;'
+ cat ../docs/rpki-db-schema.sql
+ echo 'DROP DATABASE irdb; CREATE DATABASE irdb; USE irdb;'
+ cat ../docs/sample-irbe.sql
+) |
+mysql -u root -p

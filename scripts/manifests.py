ef19bbf62eefa2aef1476c6130b2bb679881fce6
Rob Austein <sra@hactrn.net>
2007-10-09 23:59:45 +0000

    Checkpoint
    svn path=/scripts/manifests.py; revision=1145

--- scripts/manifests.py
+++ scripts/manifests.py
@@ -2,19 +2,25 @@
 
 import rpki.x509, rpki.manifest, time, glob, os
 
-show_content_1                  = True
-show_signed_manifest_PEM        = True
+show_content_1                  = False
+show_signed_manifest_PEM        = False
 show_signed_manifest_asn1dump   = True
-show_content_2                  = True
-show_content_3                  = True
+show_content_2                  = False
+show_content_3                  = False
 
 def dumpasn1(thing):
-  i,o = os.popen4(("dumpasn1", "-a", "-"))
-  i.write(thing)
-  i.close()
-  print "\n".join(x for x in o.read().splitlines() if x.startswith(" "))
-  o.close()
-
+  # Save to file rather than using popen4() because dumpasn1 uses
+  # seek() when decoding ASN.1 content nested in OCTET STRING values.
+  try:
+    fn = "dumpasn1.tmp"
+    f = open(fn, "w")
+    f.write(thing)
+    f.close()
+    f = os.popen("dumpasn1 2>&1 -a " + fn)
+    print "\n".join(x for x in f.read().splitlines() if x.startswith(" "))
+    f.close()
+  finally:
+    os.unlink(fn)
 
 m = rpki.x509.SignedManifest()
 m.build(serial = 17,

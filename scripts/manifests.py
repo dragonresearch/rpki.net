6a1daf6220fcbe044ed8a4f28e4b5ab618b00ca9
Rob Austein <sra@hactrn.net>
2007-10-09 19:25:15 +0000

    Cleanup
    svn path=/scripts/manifests.py; revision=1137

--- scripts/manifests.py
+++ scripts/manifests.py
@@ -1,15 +1,16 @@
 # $Id$
 
-import rpki.x509, rpki.manifest, time, POW, POW.pkix, glob
+import rpki.x509, rpki.manifest, time, POW, POW.pkix, glob, os
 
-def onefile(name):
-  f = open(name)
+def one_cert(filename):
+  c = rpki.x509.X509(Auto_file = filename)
   d = POW.Digest(POW.SHA256_DIGEST)
-  d.update(f.read())
-  f.close()
-  return name.rpartition("/")[2], d.digest()
+  d.update(c.get_DER())
+  return filename.rpartition("/")[2], d.digest()
 
 now = time.time()
+
+certs = glob.glob("resource-cert-samples/*.cer")
 
 mani = rpki.manifest.Manifest()
 mani.set((0,                                   # version
@@ -17,11 +18,8 @@ mani.set((0,                                   # version
           POW.pkix.time2gen(now),              # thisUpdate
           POW.pkix.time2gen(now + 24*60*60),   # nextUpdate
           (2, 16, 840, 1, 101, 3, 4, 2, 1),    # id-sha256
-          [onefile(i) for i in glob.glob("resource-cert-samples/*.cer")]))
+          [one_cert(i) for i in certs]))
 
-print mani.get()
-
-if True:
-  f = open("manifests.out.der", "w")
-  f.write(mani.toString())
-  f.close()
+f = os.popen("dumpasn1 -a - 2>/dev/null", "w")
+f.write(mani.toString())
+f.close()

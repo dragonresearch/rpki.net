96f5ab7cb0f8d696014bbc4bfc54d00b7af176d9
Rob Austein <sra@hactrn.net>
2007-10-31 00:54:30 +0000

    Encode AIA correctly
    svn path=/scripts/rpki/x509.py; revision=1210

--- scripts/rpki/x509.py
+++ scripts/rpki/x509.py
@@ -281,7 +281,7 @@ class X509(DER_object):
     exts = [ ["subjectKeyIdentifier",   False, ski],
              ["authorityKeyIdentifier", False, (aki, (), None)],
              ["cRLDistributionPoints",  False, ((("fullName", (("uri", crldp),)), None, ()),)],
-             ["authorityInfoAccess",    False, aia],
+             ["authorityInfoAccess",    False, ((1, 3, 6, 1, 5, 5, 7, 48, 2), ("uri", aia))],
              ["subjectInfoAccess",      False, sia],
              ["certificatePolicies",    True,  (((1, 3, 6, 1, 5, 5, 7, 14, 2), ()),)] ]
 

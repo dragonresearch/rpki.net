e18ffa5f2926248cc58503a18de437e8612a650e
Rob Austein <sra@hactrn.net>
2007-12-17 21:26:03 +0000

    Automate
    svn path=/scripts/irbe-setup.sh; revision=1388

---
+++ scripts/irbe-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -
+# $Id$
+#
+# Run irbe-setup.py, under screen if possible.
+
+make test
+
+if test -n "$STY"
+then
+  screen python rpkid.py
+else
+  python rpkid.py >>rpkid.log 2>&1 & rpkid=$!
+  trap "kill $rpkid" 0 1 2 3 13 15
+fi
+
+sleep 5
+
+exec python irbe-setup.py

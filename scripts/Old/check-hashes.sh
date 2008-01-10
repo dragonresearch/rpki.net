bf9db8250b00058e79ecef8f0c3afee414418926
Rob Austein <sra@hactrn.net>
2008-01-10 04:22:47 +0000

    Copyright notices
    svn path=/docs/INSTALLATION; revision=1470

--- scripts/Old/check-hashes.sh
+++ scripts/Old/check-hashes.sh
@@ -1,6 +1,20 @@
 #!/bin/sh -
 # $Id$
 
+# Copyright (C) 2007--2008  American Registry for Internet Numbers ("ARIN")
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND ARIN DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS.  IN NO EVENT SHALL ARIN BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+# OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+
 : ${openssl=/u/sra/isc/route-pki/subvert-rpki.hactrn.net/openssl/openssl/apps/openssl}
 : ${switches='-verbose -crl_check_all -policy_check -explicit_policy -policy 1.3.6.1.5.5.7.14.2 -x509_strict'}
 : ${hashtree=hashed}
--- scripts/rpki/__init__.py
+++ scripts/rpki/__init__.py
@@ -1,5 +1,19 @@
 # $Id$
 
+# Copyright (C) 2007--2008  American Registry for Internet Numbers ("ARIN")
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND ARIN DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS.  IN NO EVENT SHALL ARIN BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+# OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+
 # This file exists to tell Python that this the content of this
 # directory constitute a Python package.  Since we're not doing
 # anything exotic, this file doesn't need to contain any code, but
@@ -22,7 +36,7 @@
 ## collaboration with the other RIRs.  If you're interested in this
 ## package you might also be interested in:
 ##
-## \li <a href="http://viewvc.hactrn.net/subvert-rpki.hactrn.net/rcynic/">the rcynic validation tool</a>
-## \li <a href="http://www.hactrn.net/opaque/rcynic.html">a sample of rcynic's summary output</a>
-## \li <a href="http://mirin.apnic.net/resourcecerts/wiki/">APNIC's Wiki</a>
-## \li <a href="http://mirin.apnic.net/trac/">APNIC's project Trac instance</a>
+## @li <a href="http://viewvc.hactrn.net/subvert-rpki.hactrn.net/rcynic/">the rcynic validation tool</a>
+## @li <a href="http://www.hactrn.net/opaque/rcynic.html">a sample of rcynic's summary output</a>
+## @li <a href="http://mirin.apnic.net/resourcecerts/wiki/">APNIC's Wiki</a>
+## @li <a href="http://mirin.apnic.net/trac/">APNIC's project Trac instance</a>

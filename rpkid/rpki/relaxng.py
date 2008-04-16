f2ab9b96b8131d1463df6cc7d834fdb052615c8c
Rob Austein <sra@hactrn.net>
2008-04-16 04:59:25 +0000

    regen
    svn path=/docs/rpki-db-schema.pdf; revision=1669

--- rpkid/rpki/relaxng.py
+++ rpkid/rpki/relaxng.py
@@ -6,7 +6,7 @@ import lxml.etree
 ## Parsed RelaxNG left_right schema
 left_right = lxml.etree.RelaxNG(lxml.etree.fromstring('''<?xml version="1.0" encoding="UTF-8"?>
 <!--
-  $Id: left-right-schema.rnc 1654 2008-04-11 21:09:48Z sra $
+  $Id: left-right-schema.rng 1668 2008-04-16 04:58:58Z sra $
   
   RelaxNG (Compact Syntax) Schema for RPKI left-right protocol.
   

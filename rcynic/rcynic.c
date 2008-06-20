18b0209c26e1d20218f98c379d03c7002b7224f7
Rob Austein <sra@hactrn.net>
2008-06-20 23:33:20 +0000

    Checkpoint
    svn path=/rcynic/rcynic.c; revision=1911

--- rcynic/rcynic.c
+++ rcynic/rcynic.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006--2007  American Registry for Internet Numbers ("ARIN")
+ * Copyright (C) 2006--2008  American Registry for Internet Numbers ("ARIN")
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -271,15 +271,12 @@ typedef struct FileAndHash_st {
   ASN1_BIT_STRING *hash;
 } FileAndHash;
 
+DECLARE_STACK_OF(FileAndHash)
+
 ASN1_SEQUENCE(FileAndHash) = {
   ASN1_SIMPLE(FileAndHash, file, ASN1_IA5STRING),
   ASN1_SIMPLE(FileAndHash, hash, ASN1_BIT_STRING)
 } ASN1_SEQUENCE_END(FileAndHash)
-
-DECLARE_STACK_OF(FileAndHash)
-
-#define sk_FileAndHash_num(st)		SKM_sk_num(FileAndHash, (st))
-#define sk_FileAndHash_value(st, i)	SKM_sk_value(FileAndHash, (st), (i))
 
 typedef struct Manifest_st {
   ASN1_INTEGER *version, *manifestNumber;
@@ -302,6 +299,28 @@ DECLARE_ASN1_FUNCTIONS(Manifest)
 
 IMPLEMENT_ASN1_FUNCTIONS(FileAndHash)
 IMPLEMENT_ASN1_FUNCTIONS(Manifest)
+
+#define sk_FileAndHash_new(st)			SKM_sk_new(FileAndHash, (st))
+#define sk_FileAndHash_new_null()		SKM_sk_new_null(FileAndHash)
+#define sk_FileAndHash_free(st)			SKM_sk_free(FileAndHash, (st))
+#define sk_FileAndHash_num(st)			SKM_sk_num(FileAndHash, (st))
+#define sk_FileAndHash_value(st, i)		SKM_sk_value(FileAndHash, (st), (i))
+#define sk_FileAndHash_set(st, i, val)		SKM_sk_set(FileAndHash, (st), (i), (val))
+#define sk_FileAndHash_zero(st)			SKM_sk_zero(FileAndHash, (st))
+#define sk_FileAndHash_push(st, val)		SKM_sk_push(FileAndHash, (st), (val))
+#define sk_FileAndHash_unshift(st, val)		SKM_sk_unshift(FileAndHash, (st), (val))
+#define sk_FileAndHash_find(st, val)		SKM_sk_find(FileAndHash, (st), (val))
+#define sk_FileAndHash_find_ex(st, val)		SKM_sk_find_ex(FileAndHash, (st), (val))
+#define sk_FileAndHash_delete(st, i)		SKM_sk_delete(FileAndHash, (st), (i))
+#define sk_FileAndHash_delete_ptr(st, ptr)	SKM_sk_delete_ptr(FileAndHash, (st), (ptr))
+#define sk_FileAndHash_insert(st, val, i)	SKM_sk_insert(FileAndHash, (st), (val), (i))
+#define sk_FileAndHash_set_cmp_func(st, cmp)	SKM_sk_set_cmp_func(FileAndHash, (st), (cmp))
+#define sk_FileAndHash_dup(st)			SKM_sk_dup(FileAndHash, st)
+#define sk_FileAndHash_pop_free(st, free_func)	SKM_sk_pop_free(FileAndHash, (st), (free_func))
+#define sk_FileAndHash_shift(st)		SKM_sk_shift(FileAndHash, (st))
+#define sk_FileAndHash_pop(st)			SKM_sk_pop(FileAndHash, (st))
+#define sk_FileAndHash_sort(st)			SKM_sk_sort(FileAndHash, (st))
+#define sk_FileAndHash_is_sorted(st)		SKM_sk_is_sorted(FileAndHash, (st))
 
 
 

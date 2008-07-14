32be6b022b43f9e530d205c4b3d8806b15c6e35c
Rob Austein <sra@hactrn.net>
2008-07-14 21:05:55 +0000

    Fix copyright notices
    svn path=/utils/find_roa/test_roa.sh; revision=1989

--- utils/find_roa/test_roa.sh
+++ utils/find_roa/test_roa.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-# Copyright (C) 2006--2008  American Registry for Internet Numbers ("ARIN")
+# Copyright (C) 2008  American Registry for Internet Numbers ("ARIN")
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above

45d66ed01b1c025e2a27ef7b4ef31c5b906e539b
Rob Austein <sra@hactrn.net>
2009-04-08 01:54:08 +0000

    Cleanup
    svn path=/rtr-origin/server.sh; revision=2325

--- rtr-origin/server.sh
+++ rtr-origin/server.sh
@@ -12,12 +12,6 @@
 # fail mysteriously, leaving behind naught but a SIGCHILD log message
 # from sshd as this script dies.
 
-#/usr/bin/printenv		>> /u/sra/rpki/subvert-rpki.hactrn.net/rtr-origin/server.log
-
-#echo '[Server starting up]'	>> /u/sra/rpki/subvert-rpki.hactrn.net/rtr-origin/server.log
-
-cd /u/sra/rpki/subvert-rpki.hactrn.net/rtr-origin/
-
-#/usr/local/bin/python rtr-origin.py server >> /u/sra/rpki/subvert-rpki.hactrn.net/rtr-origin/server.log 2>&1
+cd /u/sra/rpki/subvert-rpki.hactrn.net/rtr-origin
 
 exec /usr/local/bin/python rtr-origin.py server 2>> /u/sra/rpki/subvert-rpki.hactrn.net/rtr-origin/server.log

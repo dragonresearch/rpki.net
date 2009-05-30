4448460b779c6e138b4c8e37e02bac0bd41fc1d0
Rob Austein <sra@hactrn.net>
2009-05-30 03:15:05 +0000

    Certificate verification
    svn path=/scripts/tls-client.py; revision=2476

--- scripts/tls-client.py
+++ scripts/tls-client.py
@@ -2,10 +2,20 @@
 
 import socket, POW, time
 
+key = POW.pemRead(POW.RSA_PRIVATE_KEY, open("Carol.key", "r").read())
+cer = POW.pemRead(POW.X509_CERTIFICATE, open("Carol.cer", "r").read())
+ta  = POW.pemRead(POW.X509_CERTIFICATE, open("Alice-TA.cer", "r").read())
+
 s = socket.socket()
 s.connect(('',6666))
 
 ssl = POW.Ssl(POW.TLSV1_CLIENT_METHOD)
+
+ssl.useCertificate(cer)
+ssl.useKey(key)
+ssl.setVerifyMode(POW.SSL_VERIFY_PEER | POW.SSL_VERIFY_FAIL_IF_NO_PEER_CERT)
+ssl.trustCertificate(ta)
+
 ssl.setFd(s.fileno())
 ssl.connect()
 
--- scripts/tls-server.py
+++ scripts/tls-server.py
@@ -4,8 +4,11 @@ import socket, POW, time
 
 key = POW.pemRead(POW.RSA_PRIVATE_KEY, open("Alice.key", "r").read())
 cer = POW.pemRead(POW.X509_CERTIFICATE, open("Alice.cer", "r").read())
+ta  = POW.pemRead(POW.X509_CERTIFICATE, open("Carol-TA.cer", "r").read())
 
 listener = socket.socket()
+listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 listener.bind(('',6666))
 listener.listen(5)
 
@@ -14,12 +17,17 @@ while not s:
   time.sleep(2)
   s, addr = listener.accept()
 
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
 print "Got connection %r from %r" % (s, addr)
 
 ssl = POW.Ssl(POW.TLSV1_SERVER_METHOD)
 
 ssl.useCertificate(cer)
 ssl.useKey(key)
+ssl.setVerifyMode(POW.SSL_VERIFY_PEER | POW.SSL_VERIFY_FAIL_IF_NO_PEER_CERT)
+ssl.trustCertificate(ta)
 
 ssl.setFd(s.fileno())
 ssl.accept()

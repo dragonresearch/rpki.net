3515f3253087a1d8c270b53f5d42354b7bf3b1a4
Rob Austein <sra@hactrn.net>
2009-06-17 16:25:33 +0000

    Checkpoint
    svn path=/myrpki/xml-parse-test.py; revision=2534

--- myrpki/xml-parse-test.py
+++ myrpki/xml-parse-test.py
@@ -15,11 +15,39 @@ if False:
 
 rng.assertValid(tree)
 
-for x in tree.getiterator():
-  print x
-# print x.get("handle")
+if False:
+  for x in tree.getiterator():
+    print x
+  # print x.get("handle")
+    for k, v in x.items():
+      if v:
+        print " ", k, v
+    if x.text:
+      print " ", x.text
+
+def tag(t):
+  return "{http://www.hactrn.net/uris/rpki/myrpki/}" + t
+
+print "My handle:", tree.get("handle")
+
+print "Children:"
+for x in tree.getiterator(tag("child")):
+  print x.get("handle"), x
   for k, v in x.items():
     if v:
       print " ", k, v
-  if x.text:
-    print " ", x.text
+
+print "ROA requests:"
+for x in tree.getiterator(tag("roa_request")):
+  print x
+  for k, v in x.items():
+    if v:
+      print " ", k, v
+
+ca = tree.findtext(tag("bpki_ca_certificate"))
+if ca:
+  print "CA certificate:", ca
+
+ee = tree.findtext(tag("bpki_ee_certificate"))
+if ee:
+  print "EE certificate:", ee

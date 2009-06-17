923b0364e7fac251f5146b7ea8714e12586fc084
Rob Austein <sra@hactrn.net>
2009-06-17 14:37:59 +0000

    Cleanup
    svn path=/scripts/Old/encode-test.py; revision=2532

--- scripts/testpoke.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -
-# $Id$
-#
-# Test client using APNIC's rpki_poke.pl script.
-
-: ${pokedir=../../mirin.apnic.net/rpki_engine/branches/gary-poker/client/poke}
-
-exec perl -I $pokedir $pokedir/rpki_poke.pl ${1+"$@"}

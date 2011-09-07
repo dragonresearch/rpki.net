5b0713ae34b2a3962aa91e2483f978e614c642be
Rob Austein <sra@hactrn.net>
2011-09-07 01:53:12 +0000

    Change [3245] should have applied to sql-dumper.py too.
    svn path=/rpkid/tests/sql-dumper.py; revision=3973

--- rpkid/tests/sql-dumper.py
+++ rpkid/tests/sql-dumper.py
@@ -20,13 +20,13 @@ PERFORMANCE OF THIS SOFTWARE.
 
 import subprocess, rpki.config
 
-cfg = rpki.config.parser(None, "yamltest")
+cfg = rpki.config.parser(None, "yamltest", allow_missing = True)
 
 for name in ("rpkid", "irdbd", "pubd"):
 
   username = cfg.get("%s_sql_username" % name, name[:4])
   password = cfg.get("%s_sql_password" % name, "fnord")
 
-  cmd = ["mysqldump", "-u", username, "-p" + password, "--databases", name[:4]]
+  cmd = ["mysqldump", "-u", username, "-p" + password, "--databases"]
   cmd.extend("%s%d" % (name[:4], i) for i in xrange(12))
   subprocess.check_call(cmd, stdout = open("backup.%s.sql" % name, "w"))

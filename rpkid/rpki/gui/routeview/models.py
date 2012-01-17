8e6b05e18acf519fd700a2ad5d2e088c6ccc86c9
Michael Elkins <melkins@tislabs.com>
2012-01-17 05:03:02 +0000

    fix module docstrings
    svn path=/branches/tk161/; revision=4166

--- rpkid/rpki/gui/routeview/models.py
+++ rpkid/rpki/gui/routeview/models.py
@@ -2,7 +2,7 @@ import django.db.models
 import rpki.gui.models
 
 class RouteOrigin(rpki.gui.models.PrefixV4):
-    "Represents a BGP routing table entry."
+    "Represents an IPv4 BGP routing table entry."
 
     asn = django.db.models.PositiveIntegerField(help_text='origin AS', null=False)
         
@@ -10,7 +10,7 @@ class RouteOrigin(rpki.gui.models.PrefixV4):
         return u"AS%d's route origin for %s" % (self.asn, self.get_prefix_display())
 
 class RouteOriginV6(rpki.gui.models.PrefixV6):
-    "Represents a BGP routing table entry."
+    "Represents an IPv6 BGP routing table entry."
 
     asn = django.db.models.PositiveIntegerField(help_text='origin AS', null=False)
         

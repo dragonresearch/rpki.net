6f45f4a6b5a65ab12db71623877ce075f218e98c
Rob Austein <sra@hactrn.net>
2012-04-16 19:11:35 +0000

    Add flat text and PDF translations of documentation from http://trac.rpki.net/, which is now the primary documentation source.
    This partially addresses #224, although there is no doubt still a way
    to go on content of the new documentation, given the
    complaints\\\\\\\\\\helpful suggestions I'm getting from my esteemed
    group of alpha testers.
    
    svn path=/trunk/; revision=4423

---
+++ doc/cronjob.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -
+# $Id$
+
+lock=cronjob.lock
+
+cd `/usr/bin/dirname $0` || exit
+
+case "$1" in
+
+locked)
+    exec >cronjob.log 2>&1
+    set -x
+    export PATH=/bin:/usr/bin:/usr/local/bin
+    /usr/local/bin/svn update
+    /usr/local/bin/python /u/sra/rpki/subvert-rpki.hactrn.net/trunk/buildtools/pull-doc-from-wiki.py
+    /usr/local/bin/svn status
+    if test -n "$(/usr/local/bin/svn status doc.*)"
+    then
+	/usr/local/bin/svn add --force doc.* manual.pdf
+	/usr/local/bin/svn commit --message 'Automatic pull of documentation from Wiki.' doc.* manual.pdf
+    else
+	/usr/local/bin/svn revert manual.pdf
+    fi
+    /usr/local/bin/svn update
+    ;;
+*)
+    exec /usr/bin/lockf -s -t 0 $lock "$0" locked
+    ;;
+
+esac

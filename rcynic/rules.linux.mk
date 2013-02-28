fa8491add8e4a5895ea96f5f5cb1f01273185f0e
Rob Austein <sra@hactrn.net>
2013-02-28 01:10:14 +0000

    Fix rcynic installation rules broken in packaging branch merge. Fixes #428 and 429.
    svn path=/trunk/; revision=5089

--- rcynic/rules.linux.mk
+++ rcynic/rules.linux.mk
@@ -1,7 +1,7 @@
 # $Id$
 
 install-user-and-group: .FORCE
-	@if ${AWK} -F: 'BEGIN {status = 1} $$1 == ${RCYNIC_GROUP} {status = 0} END {exit status}' /etc/group; \
+	@if getent group ${RCYNIC_GROUP} >/dev/null; \
 	then \
 	    echo "You already have a group \"${RCYNIC_GROUP}\", so I will use it."; \
 	elif /usr/sbin/groupadd ${RCYNIC_GROUP}; \
@@ -13,10 +13,10 @@ install-user-and-group: .FORCE
 	    exit 1; \
 	fi
 	@nogroup='-N'; \
-	if test -f /etc/redhat-release; then; read vendor release version < /etc/redhat-release; if test $$vendor = CentOS; then; nogroup='-n'; fi; fi; \
-	if ${AWK} -F: 'BEGIN {status = 1} $$1 == ${RCYNIC_USER} {status = 0} END {exit status}' /etc/passwd; \
+	if test -f /etc/redhat-release; then read vendor release version < /etc/redhat-release; if test $$vendor = CentOS; then nogroup='-n'; fi; fi; \
+	if getent passwd ${RCYNIC_USER} >/dev/null; \
 	then \
-	    echo "You already have a user \"${RCYNIC_USER}\", so I will use it." \
+	    echo "You already have a user \"${RCYNIC_USER}\", so I will use it."; \
 	elif /usr/sbin/useradd -g ${RCYNIC_GROUP} -M $$nogroup -d "${RCYNIC_DIR}" -s /sbin/nologin -c "${RCYNIC_GECOS}" ${RCYNIC_USER}; \
 	then \
 	    echo "Added user \"${RCYNIC_USER}\"."; \

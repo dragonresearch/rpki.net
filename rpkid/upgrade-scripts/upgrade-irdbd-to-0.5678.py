1d7168b117b15737741d962ab18d70c078b8e329
Rob Austein <sra@hactrn.net>
2014-04-02 01:48:20 +0000

    Sigh, s/--self_id/--self_handle/.
    svn path=/branches/tk671/; revision=5730

--- rpkid/upgrade-scripts/upgrade-irdbd-to-0.5678.py
+++ rpkid/upgrade-scripts/upgrade-irdbd-to-0.5678.py
@@ -48,7 +48,7 @@ handles = subprocess.check_output((rpkic, "list_self_handles")).splitlines()
 
 argv = [irbe_cli]
 for handle in handles:
-  argv.extend(("self", "--self_id", handle, "--action", "set",
+  argv.extend(("self", "--self_handle", handle, "--action", "set",
                "--rekey", "--reissue", "--run_now"))
 subprocess.check_call(argv)
 
@@ -56,7 +56,7 @@ time.sleep(10)
 
 argv = [irbe_cli]
 for handle in handles:
-  argv.extend(("self", "--self_id", handle, "--action", "set",
+  argv.extend(("self", "--self_handle", handle, "--action", "set",
                "--revoke", "--reissue", "--run_now", "--publish_world_now"))
 subprocess.check_call(argv)
 

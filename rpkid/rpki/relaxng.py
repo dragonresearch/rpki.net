0d70f79411584b4f1619afb7b0e793efead995b8
Rob Austein <sra@hactrn.net>
2014-04-04 16:33:25 +0000

    Regen
    svn path=/branches/tk671/; revision=5749

--- rpkid/rpki/relaxng.py
+++ rpkid/rpki/relaxng.py
@@ -1099,7 +1099,7 @@ left_right = lxml.etree.RelaxNG(lxml.etree.fromstring(r'''<?xml version="1.0" en
 ## Parsed RelaxNG up_down schema
 up_down = lxml.etree.RelaxNG(lxml.etree.fromstring(r'''<?xml version="1.0" encoding="UTF-8"?>
 <!--
-  $Id: up-down-schema.rnc 5746 2014-04-04 02:00:06Z sra $
+  $Id: up-down-schema.rnc 5748 2014-04-04 16:30:30Z sra $
   
   RelaxNG schema for the up-down protocol, extracted from RFC 6492.
   
@@ -1110,21 +1110,21 @@ up_down = lxml.etree.RelaxNG(lxml.etree.fromstring(r'''<?xml version="1.0" encod
   modification, are permitted provided that the following conditions
   are met:
   
-  · Redistributions of source code must retain the above copyright
+  * Redistributions of source code must retain the above copyright
     notice, this list of conditions and the following disclaimer.
   
-  · Redistributions in binary form must reproduce the above copyright
+  * Redistributions in binary form must reproduce the above copyright
     notice, this list of conditions and the following disclaimer in
     the documentation and/or other materials provided with the
     distribution.
   
-  · Neither the name of Internet Society, IETF or IETF Trust, nor the
+  * Neither the name of Internet Society, IETF or IETF Trust, nor the
     names of specific contributors, may be used to endorse or promote
     products derived from this software without specific prior written
     permission.
   
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
   COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,

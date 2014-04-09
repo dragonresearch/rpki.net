7170af54acc61fca23e72fdce0cb04633515bcf2
Rob Austein <sra@hactrn.net>
2014-04-09 01:59:19 +0000

    Tweak setup.py to conditionalize installation of rpki-ca scripts; move installation targets for Python library up from ca to top Makefile.
    svn path=/branches/tk685/; revision=5771

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 # $Id$
-#
-# Copyright (C) 2011-2013  Internet Systems Consortium ("ISC")
+# 
+# Copyright (C) 2014  Dragon Research Labs ("DRL")
+# Portions copyright (C) 2011--2013  Internet Systems Consortium ("ISC")
 # 
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
-# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-# AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT,
-# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
-# OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# copyright notices and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS" AND DRL AND ISC DISCLAIM ALL
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL DRL OR
+# ISC BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+# DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
+# OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+# TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
 from distutils.core import setup, Extension
@@ -34,60 +36,68 @@ except ImportError:
 
 # pylint: disable=W0622
 
-setup(name              = "rpkitoolkit",
-      version           = VERSION,
-      description       = "RPKI Toolkit",
-      license           = "BSD",
-      url               = "http://rpki.net/",
-      cmdclass          = {"build_scripts"   : setup_extensions.build_scripts,
-                           "install_scripts" : setup_extensions.install_scripts},
-      packages          = ["rpki",
-                           "rpki.POW",
-                           "rpki.irdb",
-                           "rpki.gui",
-                           "rpki.gui.app",
-                           "rpki.gui.cacheview",
-                           "rpki.gui.api",
-                           "rpki.gui.routeview"],
-      ext_modules       = [Extension("rpki.POW._POW", ["ext/POW.c"], 
-                                     extra_compile_args =  autoconf.CFLAGS.split(),
-                                     extra_link_args    = (autoconf.LDFLAGS + " " +
-                                                           autoconf.LIBS).split())],
-      package_data      = {"rpki.gui.app" :
-                             ["migrations/*.py",
-                              "static/*/*",
-                              "templates/*.html",
-                              "templates/*/*.html",
-                              "templatetags/*.py"],
-                           "rpki.gui.cacheview" :
-                             ["templates/*/*.html"]},
-      scripts           = [(autoconf.sbindir,
-                            ["ca/rpkic",
-                             "ca/rpki-confgen",
-                             "ca/rpki-start-servers",
-                             "ca/rpki-sql-backup",
-                             "ca/rpki-sql-setup",
-                             "ca/rpki-manage",
-                             "ca/rpkigui-query-routes",
-                             "ca/irbe_cli"]),
-                           (autoconf.libexecdir,
-                            ["ca/irdbd",
-                             "ca/pubd",
-                             "ca/rootd",
-                             "ca/rpkid",
-                             "ca/rpkigui-import-routes",
-                             "ca/rpkigui-check-expired",
-                             "ca/rpkigui-rcynic",
-                             "ca/rpkigui-apache-conf-gen"])],
-      data_files        = [(autoconf.sysconfdir  + "/rpki",
-                            ["ca/rpki-confgen.xml"]),
-                           (autoconf.datarootdir + "/rpki/wsgi",
-                            ["ca/rpki.wsgi"]),
-                           (autoconf.datarootdir + "/rpki/media/css",
-                            glob("rpki/gui/app/static/css/*")),
-                           (autoconf.datarootdir + "/rpki/media/js",
-                            glob("rpki/gui/app/static/js/*")),
-                           (autoconf.datarootdir + "/rpki/media/img",
-                            glob("rpki/gui/app/static/img/*")),
-                           (autoconf.datarootdir + "/rpki/upgrade-scripts",
-                            glob("ca/upgrade-scripts/*"))])
+setup_args = dict(
+  name          = "rpkitoolkit",
+  version       = VERSION,
+  description   = "RPKI Toolkit",
+  license       = "BSD",
+  url           = "http://rpki.net/",
+  cmdclass      = {"build_scripts"   : setup_extensions.build_scripts,
+                   "install_scripts" : setup_extensions.install_scripts},
+  packages      = ["rpki",
+                   "rpki.POW",
+                   "rpki.irdb",
+                   "rpki.gui",
+                   "rpki.gui.app",
+                   "rpki.gui.cacheview",
+                   "rpki.gui.api",
+                   "rpki.gui.routeview"],
+  ext_modules   = [Extension("rpki.POW._POW", ["ext/POW.c"], 
+                             extra_compile_args =  autoconf.CFLAGS.split(),
+                             extra_link_args    = (autoconf.LDFLAGS + " " +
+                                                   autoconf.LIBS).split())],
+  package_data  = {"rpki.gui.app" :
+                   ["migrations/*.py",
+                    "static/*/*",
+                    "templates/*.html",
+                    "templates/*/*.html",
+                    "templatetags/*.py"],
+                   "rpki.gui.cacheview" :
+                   ["templates/*/*.html"]})
+
+# Add rp stuff here too, eventually.
+
+if autoconf.CA_TARGET == "ca":
+  setup_args.update(
+    scripts     = [(autoconf.sbindir,
+                    ["ca/rpkic",
+                     "ca/rpki-confgen",
+                     "ca/rpki-start-servers",
+                     "ca/rpki-sql-backup",
+                     "ca/rpki-sql-setup",
+                     "ca/rpki-manage",
+                     "ca/rpkigui-query-routes",
+                     "ca/irbe_cli"]),
+                   (autoconf.libexecdir,
+                    ["ca/irdbd",
+                     "ca/pubd",
+                     "ca/rootd",
+                     "ca/rpkid",
+                     "ca/rpkigui-import-routes",
+                     "ca/rpkigui-check-expired",
+                     "ca/rpkigui-rcynic",
+                     "ca/rpkigui-apache-conf-gen"])],
+    data_files  = [(autoconf.sysconfdir  + "/rpki",
+                    ["ca/rpki-confgen.xml"]),
+                   (autoconf.datarootdir + "/rpki/wsgi",
+                    ["ca/rpki.wsgi"]),
+                   (autoconf.datarootdir + "/rpki/media/css",
+                    glob("rpki/gui/app/static/css/*")),
+                   (autoconf.datarootdir + "/rpki/media/js",
+                    glob("rpki/gui/app/static/js/*")),
+                   (autoconf.datarootdir + "/rpki/media/img",
+                    glob("rpki/gui/app/static/img/*")),
+                   (autoconf.datarootdir + "/rpki/upgrade-scripts",
+                    glob("ca/upgrade-scripts/*"))])
+
+setup(**setup_args)

f16aa58ab1cbe234091aee40b674e751bb5cd18c
Rob Austein <sra@hactrn.net>
2015-10-20 18:59:20 +0000

    Regen.
    svn path=/branches/tk705/; revision=6135

--- rpki/relaxng.py
+++ rpki/relaxng.py
@@ -6,7 +6,7 @@ from rpki.relaxng_parser import RelaxNGParser
 ## Parsed RelaxNG left_right schema
 left_right = RelaxNGParser(r'''<?xml version="1.0" encoding="UTF-8"?>
 <!--
-  $Id: left-right.rnc 6120 2015-10-16 04:56:43Z sra $
+  $Id: left-right.rnc 6134 2015-10-20 18:58:48Z sra $
   
   RelaxNG schema for RPKI left-right protocol.
   

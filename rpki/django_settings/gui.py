3014ff8f639fddb5e8d186feff027fdcbc4176e5
Rob Austein <sra@hactrn.net>
2016-03-28 16:54:38 +0000

    Apparently we lost DOWNLOAD_DIRECTORY while consolidating the GUI's Django settings with those of all the other ORM-using programs.
    svn path=/branches/tk705/; revision=6333

--- rpki/django_settings/gui.py
+++ rpki/django_settings/gui.py
@@ -107,6 +107,8 @@ def get_allowed_hosts():
 
 ALLOWED_HOSTS = get_allowed_hosts()
 
+DOWNLOAD_DIRECTORY = cfg.get("download-directory", "/var/tmp", section = "web_portal")
+
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     "django.template.loaders.filesystem.Loader",

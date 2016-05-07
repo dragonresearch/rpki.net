1cec1cd017f78e7c633e89d3ef1d41fa07da8d9d
Rob Austein <sra@hactrn.net>
2016-05-07 00:06:36 +0000

    Copyright notice update, first round: files that are entirely new since copyright holder of record changed.
    svn path=/branches/tk705/; revision=6425

--- potpourri/ca-unpickle.py
+++ potpourri/ca-unpickle.py
@@ -2,6 +2,21 @@
 
 # $Id$
 
+# Copyright (C) 2016  Parsons Government Services ("PARSONS")
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notices and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND PARSONS DISCLAIMS ALL
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL
+# PARSONS BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+# CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+# OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+# WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 """
 Unpickle trunk/ CA state packaged by ca-pickle and attempt to whack a
 tk705/ rpki-ca instance into an equivalent state.

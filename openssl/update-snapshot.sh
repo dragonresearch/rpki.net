1d766519ef8eb4d6214ea536044eb0333a2dba7d
Rob Austein <sra@hactrn.net>
2016-10-10 14:51:32 -0400

    Update to OpenSSL 1.0.2j.
    There would (probably) be no security issue with continuing to use
    OpenSSL 1.0.2h for RPKI, but it's usually best to stay current.
    
    Update the update-snapshot script to use git instead of svn.

--- openssl/update-snapshot.sh
+++ openssl/update-snapshot.sh
@@ -2,11 +2,11 @@
 # $Id$
 #
 # Fetch today's OpenSSL tarball and do everything needed to make it
-# the current code other than the svn commit.
+# the current code other than the git commit.
 
 #version="1.0.0-stable-SNAP-$(date +%Y%m%d)"
 
-version="1.0.2h"
+version="1.0.2j"
 
 tarball="openssl-${version}.tar.gz"
 
@@ -24,11 +24,12 @@ do
   if [ "$i" != "$tarball" ]
   then
     /bin/rm -rf "${i%.tar.gz}"
-    /usr/local/bin/svn rm "$i"
+    /usr/local/bin/git rm "$i"
   fi
 done
 
 /usr/bin/awk -v version="$version" '/^VERSION = / {$NF = version} {print}' Makefile.in >Makefile.in.$$ &&
 /bin/mv Makefile.in.$$ Makefile.in
 
-/usr/local/bin/svn add "$tarball"
+/usr/local/bin/git add -v "$tarball"
+/usr/local/bin/git add -v -u
